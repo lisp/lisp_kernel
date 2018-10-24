e2ecbc296c1e5683082147d896226edcc286c4e0
root <root@de10.dydra.com>
2018-10-25 00:07:31 +0200

    provisional lisp shell kernel works to the point of evaluation and simple error recovery

--- lisp_shell/install.py
+++ lisp_shell/install.py
@@ -6,11 +6,10 @@ import argparse
 from jupyter_client.kernelspec import KernelSpecManager
 from IPython.utils.tempdir import TemporaryDirectory
 
-kernel_json = {"argv":[sys.executable,"-m","bash_kernel", "-f", "{connection_file}"],
- "display_name":"Bash",
- "language":"bash",
- "codemirror_mode":"shell",
- "env":{"PS1": "$"}
+kernel_json = {"argv":[sys.executable,"-m","lisp_shell", "-f", "{connection_file}"],
+ "display_name":"Lisp",
+ "language":"lisp",
+ "codemirror_mode":"text/x-common-lisp"
 }
 
 def install_my_kernel_spec(user=True, prefix=None):
@@ -21,7 +20,7 @@ def install_my_kernel_spec(user=True, prefix=None):
         # TODO: Copy resources once they're specified
 
         print('Installing IPython kernel spec')
-        KernelSpecManager().install_kernel_spec(td, 'bash', user=user, replace=True, prefix=prefix)
+        KernelSpecManager().install_kernel_spec(td, 'lisp_shell', user=user, replace=True, prefix=prefix)
 
 def _is_root():
     try:
@@ -31,7 +30,7 @@ def _is_root():
 
 def main(argv=None):
     parser = argparse.ArgumentParser(
-        description='Install KernelSpec for Bash Kernel'
+        description='Install KernelSpec for Lisp Shell Kernel'
     )
     prefix_locations = parser.add_mutually_exclusive_group()
 
--- lisp_shell/kernel.py
+++ lisp_shell/kernel.py
@@ -7,6 +7,7 @@ import os.path
 
 import re
 import signal
+import syslog
 
 __version__ = '0.0.1'
 
@@ -21,7 +22,6 @@ from .images import (
 class LispKernel(Kernel):
     implementation = 'lisp_kernel'
     implementation_version = __version__
-    executable = 'lisp'
 
     @property
     def language_version(self):
@@ -33,7 +33,7 @@ class LispKernel(Kernel):
     @property
     def banner(self):
         if self._banner is None:
-            self._banner = check_output([executable, '--version']).decode('utf-8')
+            self._banner = check_output(['lisp', '--version']).decode('utf-8')
         return self._banner
 
     language_info = {'name': 'bash',
@@ -49,47 +49,67 @@ class LispKernel(Kernel):
         # Signal handlers are inherited by forked processes, and we can't easily
         # reset it from the subprocess. Since kernelapp ignores SIGINT except in
         # message handlers, we need to temporarily reset the SIGINT handler here
-        # so that bash and its children are interruptible.
+        # so that the child and its children are interruptible.
+        syslog.openlog('kernel')
         sig = signal.signal(signal.SIGINT, signal.SIG_DFL)
         try:
             # Note: the next few lines mirror functionality in the
             # bash() function of pexpect/replwrap.py.  Look at the
             # source code there for comments and context for
             # understanding the code here.
-            child = pexpect.spawn(executable, ['--lispinit', 'init.sxp'], echo=False,
-                                  encoding='utf-8', codec_errors='replace')
-            ps1 = replwrap.PEXPECT_PROMPT[:5] + u'\[\]' + replwrap.PEXPECT_PROMPT[5:]
-            ps2 = replwrap.PEXPECT_CONTINUATION_PROMPT[:5] + u'\[\]' + replwrap.PEXPECT_CONTINUATION_PROMPT[5:]
-            prompt_change = u"PS1='{0}' PS2='{1}' PROMPT_COMMAND=''".format(ps1, ps2)
-
-            # Using IREPLWrapper to get incremental output
-            self.lispwrapper = REPLWrapper(child, u'\>', '',
-                                            line_output_callback=self.process_output)
+            self.child = pexpect.spawn('lisp', ['--lispinit', '/opt/spocq/init.sxp'], echo=False,
+                                       encoding='utf-8', codec_errors='replace')
+            self.lispwrapper = replwrap.REPLWrapper(self.child, u'\*', None, '', '')
+            self.child.expect_exact('* ', -1)
         finally:
             signal.signal(signal.SIGINT, sig)
 
 
     def process_output(self, output):
+        syslog.syslog(output)
         if not self.silent:
             # Send standard output
             stream_content = {'name': 'stdout', 'text': output}
             self.send_response(self.iopub_socket, 'stream', stream_content)
 
 
+    def run_command(self, code):
+        # Split up multiline commands and feed them in bit-by-bit
+        # in order to avoid buffer size limit
+        res = []
+        cmdlines = code.splitlines()
+        if not cmdlines:
+            raise ValueError("No command was given")
+        syslog.syslog('run_command(' + code + ')')
+
+        for line in cmdlines:
+            syslog.syslog('run_command.line(' + line + ')')
+            self.child.sendline(line)
+        syslog.syslog('run_command.pad')
+        self.child.sendline('')
+        mode = self.child.expect(['\\* ', '\\d*] '], -1)
+        self.process_output(self.child.before)
+        if mode == 1 :
+          syslog.syslog('run_command: abort')
+          self.child.sendline(':abort')
+          mode = self.child.expect('\\* ', -1)
+          self.process_output(self.child.before)
+                                 
     def do_execute(self, code, silent, store_history=True,
                    user_expressions=None, allow_stdin=False):
         self.silent = silent
         if not code.strip():
             return {'status': 'ok', 'execution_count': self.execution_count,
                     'payload': [], 'user_expressions': {}}
-
+        syslog.syslog('do_execute(' + code + ')')
         interrupted = False
         try:
             # Note: timeout=None tells IREPLWrapper to do incremental
             # output.  Also note that the return value from
             # run_command is not needed, because the output was
             # already sent by IREPLWrapper.
-            self.lispwrapper.run_command(code.rstrip(), timeout=None)
+            # self.lispwrapper.run_command(code, timeout=None)
+            self.run_command(code)
         except KeyboardInterrupt:
             self.lispwrapper.child.sendintr()
             interrupted = True
@@ -97,30 +117,19 @@ class LispKernel(Kernel):
             output = self.lispwrapper.child.before
             self.process_output(output)
         except EOF:
-            output = self.lispwrapper.child.before + 'Restarting Bash'
-            self._start_bash()
+            output = self.lispwrapper.child.before + 'Restarting Lisp'
+            self._start_lisp()
             self.process_output(output)
 
         if interrupted:
             return {'status': 'abort', 'execution_count': self.execution_count}
 
-        try:
-            exitcode = int(self.lispwrapper.run_command('echo $?').rstrip())
-        except Exception:
-            exitcode = 1
-
-        if exitcode:
-            error_content = {'execution_count': self.execution_count,
-                             'ename': '', 'evalue': str(exitcode), 'traceback': []}
-
-            self.send_response(self.iopub_socket, 'error', error_content)
-            error_content['status'] = 'error'
-            return error_content
-        else:
-            return {'status': 'ok', 'execution_count': self.execution_count,
-                    'payload': [], 'user_expressions': {}}
+        syslog.syslog('return ok')
+        return {'status': 'ok', 'execution_count': self.execution_count,
+                'payload': [], 'user_expressions': {}}
 
     def do_complete(self, code, cursor_pos):
+        syslog.syslog('do_complete(' + code + ')')
         code = code[:cursor_pos]
         default = {'matches': [], 'cursor_start': 0,
                    'cursor_end': cursor_pos, 'metadata': dict(),
